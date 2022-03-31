6fa9119b2c097c230d14656e98ce4d0628e09783
Paula <paulabremenkamp@gmail.com>
2022-03-31 10:34:05 -0300

    created reddit object

---
+++ praw.py
@@ -0,0 +1,11 @@
+import praw
+
+#create a reddit object
+reddit = praw.Reddit(
+    client_id ='wbzA3Ai8w2dT3MAltsMG-g', 
+    client_secret ='jzeAgrSIgEzTPoCGZXjuyVaESvRCHw', 
+    user_agent ='coldplay 1.0 by /u/kampuzzle', 
+    username ='kampuzzle', 
+    password ='machinelearningcourse'
+) 
+
